534b65b781e41446f2cd179ee924d29bdabd9783
Jakub Wujec <kuba1302@gmail.com>
2023-12-05 21:38:06 +0100

    working solution

--- solutions/day_1/solution.py
+++ solutions/day_1/solution.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-input_path = Path(__file__).parents[0] / "input1.txt"
+input_path = Path(__file__).parents[0] / "input.txt"
 
 def read_txt(input_path: Path = input_path) -> list[str]:
     with open(input_path, "r") as file:
@@ -10,51 +10,36 @@ def read_txt(input_path: Path = input_path) -> list[str]:
 
 
 number_mappings_by_len = {
-    3: {
     "one": 1,
     "two": 2,
-    "six": 6,
-    },
-    4: {
+    "three": 3,
     "four": 4,
     "five": 5,
-    "nine": 9
-    },
-    5: {
-    "three": 3,
+    "six": 6,
     "seven": 7,
     "eight": 8,
-    }
+    "nine": 9,
 }
 
-def try_get_value_by_string(data_input, i, max_index):
-    for string_len in number_mappings_by_len:
-        if i + string_len - 1 > max_index:
-            i += 1
-            return None, i
 
-        if data_input[i:i+string_len] in number_mappings_by_len[string_len]:
-            value = number_mappings_by_len[string_len][data_input[i:i+string_len]]
-            i += string_len
-            return value, i
-    
-    i += 1
-    return None, i
+
     
         
 def get_numbers_from_sting(data_input: str):
     i = 0
     numbers = []
     max_index = len(data_input) - 1
+        
     while i <= max_index:
         if data_input[i].isnumeric():
             numbers.append(int(data_input[i]))
-            i += 1
+        
+        for key, val in number_mappings_by_len.items(): 
+            if data_input[i:].startswith(key):
+                numbers.append(val)
+                break
             
-        else:
-            value, i = try_get_value_by_string(data_input, i, max_index)
-            if value:
-                numbers.append(int(value))
+        i += 1
     
     return numbers
                 
@@ -65,8 +50,6 @@ def solution():
     for data_input in data:
         numbers = get_numbers_from_sting(data_input)
         first_val, last_val = numbers[0], numbers[-1]
-        print(data_input)
-        print(int(f"{first_val}{last_val}"))
         sum += int(f"{first_val}{last_val}")
     
     return sum
