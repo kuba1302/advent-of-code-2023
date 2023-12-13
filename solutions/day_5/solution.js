38b86f302686f61b8906fc1e4197d24a2f8facab
kuba1302 <kuba1302@gmail.com>
2023-12-13 01:12:02 +0100

    Almost working task2

--- solutions/day_5/solution.js
+++ solutions/day_5/solution.js
@@ -80,23 +80,13 @@ const performMapping = ({
   return returnValue ? returnValue : sourceValue;
 };
 
-const mapsOrder = [
-  "seed-to-soil",
-  "soil-to-fertilizer",
-  "fertilizer-to-water",
-  "water-to-light",
-  "light-to-temperature",
-  "temperature-to-humidity",
-  "humidity-to-location",
-];
-
-const runTask = () => {
+const runTask1 = () => {
   const input = readInputFile();
   const splittedStrings = splitByValue(input);
   const arrays = splitByValue(splittedStrings, " ")[0];
   const mappedDicts = mapIntoDict(arrays);
 
-  let lowestLocation = Infinity; // a very large number
+  let lowestLocation = Infinity;
 
   for (let seed of mappedDicts["seeds"]) {
     let currentValue = seed;
@@ -114,8 +104,138 @@ const runTask = () => {
   return lowestLocation;
 };
 
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) return "Underflow";
+    return this.items.shift();
+  }
+
+  front() {
+    if (this.isEmpty()) return "No elements in Queue";
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length == 0;
+  }
+}
+
+const performRangeMapping = ({
+  inputRanges,
+  destinationRanges,
+  sourceRanges,
+  lengthRanges,
+}) => {
+  let queue = new Queue();
+  for (let range of inputRanges) {
+    queue.enqueue(range);
+  }
+  let outputRanges = [];
+
+  while (!queue.isEmpty()) {
+    const rangeToCheck = queue.dequeue();
+    for (let i = 0; i < destinationRanges.length; i++) {
+        console.log({rangeToCheck, sourceRange: sourceRanges[i], lengthRange: lengthRanges[i]});
+
+      if (
+        (rangeToCheck.lower >= sourceRanges[i] &&
+          rangeToCheck.lower <= sourceRanges[i] + lengthRanges[i]) ||
+        (rangeToCheck.upper >= sourceRanges[i] &&
+          rangeToCheck.upper <= sourceRanges[i] + lengthRanges[i])
+      ) {
+        let intersectedRange = {
+          lower: Math.max(rangeToCheck.lower, sourceRanges[i]),
+          upper: Math.min(
+            sourceRanges[i] + lengthRanges[i],
+            rangeToCheck.upper
+          ),
+        };
+        console.log({intersectedRange: intersectedRange});
+        let mappedRange = {
+            lower: destinationRanges[i] + (intersectedRange.lower - sourceRanges[i]),
+            upper: destinationRanges[i] + (intersectedRange.upper - sourceRanges[i]),
+          };
+        outputRanges.push(mappedRange);
+
+        if (!(rangeToCheck.lower >= intersectedRange.lower && rangeToCheck.upper <= intersectedRange.upper)) {
+            
+          if (rangeToCheck.lower < intersectedRange.lower) {
+            queue.enqueue({
+              lower: rangeToCheck.lower,
+              upper: intersectedRange.lower,
+            });
+          }
+          if (rangeToCheck.upper > intersectedRange.upper) {
+            queue.enqueue({
+              lower: intersectedRange.upper,
+              upper: rangeToCheck.upper,
+            });
+          }
+        }
+      }
+    }
+  }
+
+  return outputRanges;
+};
+
+const mapsOrder = [
+  "seed-to-soil",
+  "soil-to-fertilizer",
+  "fertilizer-to-water",
+  "water-to-light",
+  "light-to-temperature",
+  "temperature-to-humidity",
+  "humidity-to-location",
+];
+
+const runTask2 = () => {
+  const input = readInputFile();
+  const splittedStrings = splitByValue(input);
+  const arrays = splitByValue(splittedStrings, " ")[0];
+  const mappedDicts = mapIntoDict(arrays);
+
+  let lowestLocation = Infinity; // a very large number
+  const seeds = mappedDicts["seeds"];
+
+  for (let i = 0; i < seeds.length; i += 2) {
+    let sourceInputStart = seeds[i];
+    let sourceInputRange = seeds[i + 1];
+
+    let currentListOfMappings = [
+      {
+        lower: sourceInputStart,
+        upper: sourceInputStart + sourceInputRange,
+      },
+    ];
+    console.log(currentListOfMappings);
+    for (let map of mapsOrder) {
+      currentListOfMappings = performRangeMapping({
+        inputRanges: currentListOfMappings,
+        ...mappedDicts["mappings"][map],
+      });
+      console.log(currentListOfMappings);
+    }
+    for (let mapping of currentListOfMappings) {
+        if (mapping.lower < lowestLocation) {
+          lowestLocation = mapping.lower;
+        }
+      }
+  }
+
+  return lowestLocation;
+};
+
 function main() {
-  const lowestLocation = runTask();
+  const lowestLocation = runTask2();
   console.log(lowestLocation);
 }
 
